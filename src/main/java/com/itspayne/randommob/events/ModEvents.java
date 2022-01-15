39899215f2310e1e1db010aca23129f00a54d88f
ItsPayne <real.teenluck@gmail.com>
2022-01-15 16:31:32 +0700

    Changed project from Eclipse to IntelliJ IDEA, added more code.

--- src/main/java/com/itspayne/randommob/events/ModEvents.java
+++ src/main/java/com/itspayne/randommob/events/ModEvents.java
@@ -2,12 +2,22 @@ package com.itspayne.randommob.events;
 
 import com.itspayne.randommob.RandomMob;
 
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
 
 @EventBusSubscriber(modid = RandomMob.MOD_ID, bus = Bus.FORGE)
 public class ModEvents {
@@ -16,10 +26,29 @@ public class ModEvents {
 	@SubscribeEvent
 	public static void onPlayerInteract(PlayerTickEvent e) {
 		Player p = e.player;
-		
+		Level world = e.player.level;
+		List<LivingEntity> entities = world.getEntitiesOfClass(LivingEntity.class, p.getBoundingBox().expandTowards(200, 200, 200));
+
 		if (e.phase == Phase.END) {
-			if (p.tickCount % 20 == 0) {
-				
+			if (p.tickCount % 20 == 0 && e.side.isClient()) {
+				for (LivingEntity livingEntity : entities){
+					if (!(livingEntity instanceof Player)) {
+						double entityX = livingEntity.getX();
+						double entityY = livingEntity.getY();
+						double entityZ = livingEntity.getZ();
+
+						CompoundTag compoundtag = new CompoundTag();
+						compoundtag.putString("id", "minecraft:chicken");
+
+						livingEntity.kill();
+
+						Entity newEntity = EntityType.loadEntityRecursive(new CompoundTag(), world, (ent) -> {
+							ent.moveTo(entityX, entityY, entityZ);
+							return ent;
+						});
+						world.addFreshEntity(newEntity);
+					}
+				}
 			}
 		}
 	}
