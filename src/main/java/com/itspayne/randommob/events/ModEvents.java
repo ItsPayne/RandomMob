dab4354c30dccccef7148bfc6fd70097d11a3e89
ItsPayne <real.teenluck@gmail.com>
2022-01-16 10:52:51 +0700

    Changed Forge and Parchment mappings versions to the latest ones.

--- src/main/java/com/itspayne/randommob/events/ModEvents.java
+++ src/main/java/com/itspayne/randommob/events/ModEvents.java
@@ -2,10 +2,7 @@ package com.itspayne.randommob.events;
 
 import com.itspayne.randommob.RandomMob;
 
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
@@ -15,14 +12,10 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Locale;
 
 @EventBusSubscriber(modid = RandomMob.MOD_ID, bus = Bus.FORGE)
 public class ModEvents {
-	public static int ticks = 0;
-	
 	@SubscribeEvent
 	public static void onPlayerInteract(PlayerTickEvent e) {
 		Player p = e.player;
@@ -31,23 +24,18 @@ public class ModEvents {
 
 		if (e.phase == Phase.END) {
 			if (p.tickCount % 20 == 0 && e.side.isClient()) {
-				for (LivingEntity livingEntity : entities){
-					if (!(livingEntity instanceof Player)) {
-						double entityX = livingEntity.getX();
-						double entityY = livingEntity.getY();
-						double entityZ = livingEntity.getZ();
+					for (LivingEntity livingEntity : entities){
+						if (!(livingEntity instanceof Player)){
+							double eX = livingEntity.getX();
+							double eY = livingEntity.getY();
+							double eZ = livingEntity.getZ();
 
-						CompoundTag compoundtag = new CompoundTag();
-						compoundtag.putString("id", "minecraft:chicken");
+							livingEntity.remove(Entity.RemovalReason.KILLED);
 
-						livingEntity.kill();
+							Entity newEntity;
+							// spawning a random one here
 
-						Entity newEntity = EntityType.loadEntityRecursive(new CompoundTag(), world, (ent) -> {
-							ent.moveTo(entityX, entityY, entityZ);
-							return ent;
-						});
-						world.addFreshEntity(newEntity);
-					}
+						}
 				}
 			}
 		}
